abd5c395d6b3147137ac004315cc3b216dab0dca
oleg chilikin <chilikinow@gmail.com>
2022-12-10 22:47:00 +0300

    refactoring

--- src/test/java/com/qaguru/lesson5/FilesTest.java
+++ src/test/java/com/qaguru/lesson5/FilesTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
